81a7f5082c1ae017bea92bd3bceaab1613ee96d4
Nicole Bushong <nbushong@jumpclouds-mbp.lan>
2022-07-06 14:29:18 -0500

    test

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,6 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Pentacles" {
 		t.Errorf("Expected last card of King of Pentacles, but got %v", d[len(d)-1])
 	}
+}
 
-	
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 78 {
+		t.Errorf("Expected deck length of 78, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
