94bb1e3826326f9fbe37939a3f403a5eb778e653
Nicole Bushong <nbushong@jumpclouds-mbp.lan>
2022-07-06 11:32:01 -0500

    read from file

--- main.go
+++ main.go
@@ -3,12 +3,7 @@ package main
 // import "fmt"
 
 func main() {
-	// cards := newDeck()
 
-	// spread, remainingCards := deal(cards, 5)
-
-	// spread.print()
-	// remainingCards.print()
-	cards := newDeck()
-	cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
